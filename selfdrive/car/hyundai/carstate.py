1148b712ab68f421b2aaf4b80ea43e509850e501
gomtings <33356618+gomtings@users.noreply.github.com>
2020-12-28 17:39:21 +0900

    fix_init_gear

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -36,7 +36,7 @@ class CarState(CarStateBase):
     self.cruiseState_enabled = False
     self.cruiseState_speed = 0
     
-    self.gear_shifter = 0
+    self.gear_shifter = GearShifter.drive # Gear_init for Nexo
 
   def update(self, cp, cp2, cp_cam):
     cp_mdps = cp2 if self.mdps_bus else cp
