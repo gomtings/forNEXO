18d38df389c736e57864a2ce57985d1f052f9b95
gomtings <33356618+gomtings@users.noreply.github.com>
2021-02-02 23:43:38 +0900

    LANE_CHANGE_TIME_MAX   10->15

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -21,7 +21,7 @@ LaneChangeDirection = log.PathPlan.LaneChangeDirection
 LOG_MPC = os.environ.get('LOG_MPC', False)
 
 LANE_CHANGE_SPEED_MIN = 50 * CV.KPH_TO_MS
-LANE_CHANGE_TIME_MAX = 10.
+LANE_CHANGE_TIME_MAX = 15.
 
 DESIRES = {
   LaneChangeDirection.none: {
